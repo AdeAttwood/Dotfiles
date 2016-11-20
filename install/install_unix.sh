e8f73bb3a3275cb48ee056497e10eac14285b5e6
AdeAttwood <attwood16@googlemail.com>
2016-11-20 07:40:34 +0000

    Add some bin files Add selenium server change some vim plugins

--- install/install_unix.sh
+++ install/install_unix.sh
@@ -6,6 +6,7 @@ ln -sf ~/.dotfiles/dotfiles/bashrc ~/.bashrc
 ln -sf ~/.dotfiles/dotfiles/tmux.conf ~/.tmux.conf
 ln -sf ~/.dotfiles/dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/dotfiles/Xresources ~/.Xresources
+ln -sf ~/.dotfiles/dotfiles/zshrc ~/.zshrc
 
 # Link to the tmuxinator dir
 rm -rf ~/.tmuxinator
@@ -17,6 +18,10 @@ sudo ln -sf ~/.dotfiles/dotfiles/bin/* /usr/local/bin
 # Link to vim plugin autoloader
 mkdir -p ~/.vim/autoload
 ln -sf ~/.dotfiles/dotfiles/vim/autoload/pathogen.vim ~/.vim/autoload/pathogen.vim
+
+# selenium server
+rm -rf ~/.selenium-server
+ln -sf ~/.dotfiles/dotfiles/selenium-server ~/.selenium-server
 
 # Install vim plugins
 rm -rf ~/.vim/bundle
