12e2d3e841981f1e36d7f7354db30c2c946d0286
Ade Attwood <code@adeattwood.co.uk>
2020-09-21 20:09:37 +0100

    fix(eamcs): change repo url and remove spacemacs dotfile
    Change the git source for spacemacs so it links up with the existing git source.
    Also remove the spacemacs dotfile, this will be in the private repo until I can
    sort out a private spacemacs module to put some stuff

--- site-modules/core/manifests/emacs.pp
+++ site-modules/core/manifests/emacs.pp
@@ -1,13 +1,8 @@
 class core::emacs {
   vcsrepo { "${user_home}/.emacs.d":
-    source   => 'https://github.com/syl20bnr/spacemacs.git',
+    source   => 'https://github.com/syl20bnr/spacemacs',
     provider =>  'git',
     user     => $user,
     revision => 'develop',
-  }
-
-  file { "${user_home}/.spacemacs":
-    ensure => 'link',
-    target => find_file('core/spacemacs'),
   }
 }
