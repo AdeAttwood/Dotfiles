9e96a5c8c8043ce8cdd21461712a8a9c18670d73
Ade Attwood <code@adeattwood.co.uk>
2020-10-13 20:46:56 +0100

    fix(core): only install vscode extension if its not already installed
    When installing a vscode extention the shell command to install the extention
    would run every time puppet was run. Now we test to see if the extention is
    installed first before trying to install it.

--- site-modules/vscode/manifests/extention.pp
+++ site-modules/vscode/manifests/extention.pp
@@ -2,6 +2,7 @@ define vscode::extention {
   exec { "code_install_${title}":
     command  => "code --install-extension ${title}",
     path     => '/usr/bin:/usr/local/bin:/usr/sbin:/bin',
+    unless   => "code --list-extensions | grep ${title}",
     provider => shell,
   }
 }
