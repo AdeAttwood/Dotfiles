d3a2f6dbe389551dcd5edad1b7d8dbfd06866190
Ade Attwood <code@adeattwood.co.uk>
2020-11-22 21:03:09 +0000

    fix(emacs): update spacemacs to the develop branch via puppet
    Before puppet was only checking to see if the repo was present. Now it is
    checking to see if the branch is updated with the develop branch and getting
    auto update when the puppet manifests are applied

--- site-modules/core/manifests/emacs.pp
+++ site-modules/core/manifests/emacs.pp
@@ -1,6 +1,7 @@
 class core::emacs {
   vcsrepo { "${user_home}/.emacs.d":
     source   => 'https://github.com/syl20bnr/spacemacs',
+    ensure   => 'latest',
     provider =>  'git',
     user     => $user,
     revision => 'develop',
