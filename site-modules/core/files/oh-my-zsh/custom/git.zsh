385829ea8b9fc8f8806ca5a942f4174410160509
Ade Attwood <ade@practically.io>
2021-07-10 21:21:24 +0100

    fix(shell): re alias gs to gss
    Now the `gs` command has be aliased to a git status alias. The original `gs`
    would open up ghost script and I would open it everyday trying to run git
    status.

--- site-modules/core/files/oh-my-zsh/custom/git.zsh
+++ site-modules/core/files/oh-my-zsh/custom/git.zsh
@@ -4,6 +4,7 @@ alias gcon="git diff --name-only --diff-filter=U"
 alias gml="git merge --log"
 alias gl="git --no-pager log --oneline --reverse --max-count=40"
 alias gap="git add -p"
+alias gs="gss"
 
 function git_get_fetch_url() {
     git remote show origin -n | awk '/Fetch URL:/{print $3}'
