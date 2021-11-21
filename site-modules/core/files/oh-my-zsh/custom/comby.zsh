a064375c2b090e9c3f608356515a77692a3547f6
Ade Attwood <ade@practically.io>
2021-11-21 11:01:40 +0000

    feat(shell): add comby shell aliases
    The main reason for this is to automatically exclude common directories form the
    search and replace to speed things up a bit.

---
+++ site-modules/core/files/oh-my-zsh/custom/comby.zsh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+#
+# Alias for comby cli tool
+#
+# See: https://comby.dev/
+#
+# Author:  Ade Attwood <code@adeattwood.co.uk>
+# Updated: 2021-11-21
+#
+
+alias comby="comby -exclude-dir vendor,node_modules,.git,.log,logs,tmp,runtime,tests/_output"
