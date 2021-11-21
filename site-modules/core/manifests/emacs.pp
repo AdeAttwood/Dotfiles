060fa3d5f991d4d34eb5bee22268d7a8c3ec9bed
Ade Attwood <ade@practically.io>
2021-11-21 19:11:44 +0000

    refactor(emacs): update folder structure to move melpa out of the repo
    When I was linking the whole .emacs.d directory all of the junk that gets put
    into that directory was getting put into the dotfiles repo and accessible by
    puppet. This was slowing things down quite alot. Now I am only linking the
    directories I need into the .emacs.d

--- site-modules/core/manifests/emacs.pp
+++ site-modules/core/manifests/emacs.pp
@@ -1,6 +1,20 @@
 class core::emacs {
   file { "${user_home}/.emacs.d":
+    ensure => 'directory',
+  }
+
+  file { "${user_home}/.emacs.d/src":
     ensure => 'link',
-    target => find_file('core/emacs'),
+    target => find_file('core/emacs/src'),
+  }
+
+  file { "${user_home}/.emacs.d/snippets":
+    ensure => 'link',
+    target => find_file('core/emacs/snippets'),
+  }
+
+  file { "${user_home}/.emacs.d/init.el":
+    ensure => 'link',
+    target => find_file('core/emacs/init.el'),
   }
 }
