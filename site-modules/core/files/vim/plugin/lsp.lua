1f9440bd307282dd46cf5d7da4a7ee90eb316d5d
Ade Attwood <adrian.attwood@marketdojo.com>
2022-07-25 20:00:49 +0100

    feat(vim): add solargraph lsp to start the ruby stuff

--- site-modules/core/files/vim/plugin/lsp.lua
+++ site-modules/core/files/vim/plugin/lsp.lua
@@ -23,7 +23,7 @@ end
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local servers = { 'tsserver', 'intelephense', 'psalm', 'gopls',  'html', 'cssls' }
+local servers = { 'tsserver', 'intelephense', 'psalm', 'gopls',  'html', 'cssls', 'solargraph' }
 for _, lsp in ipairs(servers) do
     lspconfig[lsp].setup({
         on_attach = on_attach,
