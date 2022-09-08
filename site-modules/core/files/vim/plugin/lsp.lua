12c916817a54a53167a5205ef1c63528ca071c86
Ade Attwood <adrian.attwood@marketdojo.com>
2022-09-08 20:50:59 +0100

    fix(vim): remove conflict keymap for lsp
    This keymap is conflicting with switching windows up. This is baked into
    my workflow with tmux navigator and vim

--- site-modules/core/files/vim/plugin/lsp.lua
+++ site-modules/core/files/vim/plugin/lsp.lua
@@ -13,7 +13,6 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 
   -- TODO(ade): Sort out space. This will conflict with the leader key
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
