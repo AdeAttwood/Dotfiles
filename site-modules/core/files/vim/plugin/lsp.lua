615413d6e25dd5118a18942d673499d74138328a
Ade Attwood <adrian.attwood@marketdojo.com>
2022-12-22 20:41:30 +0000

    style(vim): add lua style and luacheck and format lsp config again

--- site-modules/core/files/vim/plugin/lsp.lua
+++ site-modules/core/files/vim/plugin/lsp.lua
@@ -1,57 +1,57 @@
-local lspconfig = require("lspconfig")
+local lspconfig = require "lspconfig"
 
 local servers = {
-	-- Language servers for the day to day web development, could probably think
-	-- about loosing the html and css one and living with typescript and emmet
-	tsserver = {},
-	html = {},
-	cssls = {},
-	emmet_ls = {
-		filetypes = { "html", "typescriptreact", "javascriptreact", "css", "scss", "eruby" },
-	},
-	-- Ruby
-	solargraph = {},
-	-- Rust
-	rust_analyzer = {},
-	-- Lua for the vim config and plugin dev
-	sumneko_lua = {
-		settings = {
-			Lua = {
-				runtime = { version = "LuaJIT" },
-				diagnostics = { globals = { "vim" } },
-				workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-				telemetry = { enable = false },
-			},
-		},
-	},
+  -- Language servers for the day to day web development, could probably think
+  -- about loosing the html and css one and living with typescript and emmet
+  tsserver = {},
+  html = {},
+  cssls = {},
+  emmet_ls = {
+    filetypes = { "html", "typescriptreact", "javascriptreact", "css", "scss", "eruby" },
+  },
+  -- Ruby
+  solargraph = {},
+  -- Rust
+  rust_analyzer = {},
+  -- Lua for the vim config and plugin dev
+  sumneko_lua = {
+    settings = {
+      Lua = {
+        runtime = { version = "LuaJIT" },
+        diagnostics = { globals = { "vim" } },
+        workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+        telemetry = { enable = false },
+      },
+    },
+  },
 }
 
 local on_attach = function(_, bufnr)
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	local opts = { noremap = true, silent = true }
+  local opts = { noremap = true, silent = true }
 
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
 
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-	vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
 end
 
 local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
 for lsp, config in pairs(servers) do
-	config["on_attach"] = on_attach
-	config["capabilities"] = capabilities
-	config["init_options"] = { usePlaceholders = true }
+  config["on_attach"] = on_attach
+  config["capabilities"] = capabilities
+  config["init_options"] = { usePlaceholders = true }
 
-	lspconfig[lsp].setup(config)
+  lspconfig[lsp].setup(config)
 end
 
 -- Change the diagnostic signs
@@ -59,6 +59,6 @@ vim.fn.sign_define("DiagnosticSignHint", { text = "➤", texthl = "DiagnosticSig
 vim.fn.sign_define("DiagnosticSignInfo", { text = "ℹ", texthl = "DiagnosticSignInfo", numhl = "DiagnosticSignInfo" })
 vim.fn.sign_define("DiagnosticSignWarn", { text = "⚠", texthl = "DiagnosticSignWarn", numhl = "DiagnosticSignWarn" })
 vim.fn.sign_define(
-	"DiagnosticSignError",
-	{ text = "✖", texthl = "DiagnosticSignError", numhl = "DiagnosticSignError" }
+  "DiagnosticSignError",
+  { text = "✖", texthl = "DiagnosticSignError", numhl = "DiagnosticSignError" }
 )
