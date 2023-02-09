d1fe1f3c8799f039233bc2ea3659523c91d10941
Ade Attwood <adrian.attwood@marketdojo.com>
2023-02-09 20:22:40 +0000

    feat(pp): update delta diff

--- site-modules/core/manifests/delta.pp
+++ site-modules/core/manifests/delta.pp
@@ -4,16 +4,16 @@
 class core::delta {
   archive { 'delta':
     path          => '/tmp/delta.tar.gz',
-    source        => 'https://github.com/dandavison/delta/releases/download/0.13.0/delta-0.13.0-x86_64-unknown-linux-gnu.tar.gz',
+    source        => 'https://github.com/dandavison/delta/releases/download/0.15.1/delta-0.15.1-x86_64-unknown-linux-gnu.tar.gz',
     extract       => true,
     extract_path  => "/tmp",
     checksum_type => 'sha256',
-    checksum      => '69ddecc92e86b77016692589c094bf3a32e7cb8a0dfd9f97f253c173cce830e9',
+    checksum      => '9d9b42a7b9ea554b4bf123f245d5f2014478ac87ddc508744a3f34f5c2d512c7',
   }
 
   file { 'delta':
     path   => "${user_home}/.local/bin/delta",
     mode   => '0755',
-    source => "/tmp/delta-0.13.0-x86_64-unknown-linux-gnu/delta"
+    source => "/tmp/delta-0.15.1-x86_64-unknown-linux-gnu/delta"
   }
 }
