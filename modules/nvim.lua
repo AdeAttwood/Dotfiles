1a478525104d084d9358423940e2c92903b3e3be
Ade Attwood <code@adeattwood.co.uk>
2023-06-05 08:57:52 +0100

    fix(vim): add link in for vim snippets

--- modules/nvim.lua
+++ modules/nvim.lua
@@ -1,6 +1,11 @@
 local git = require('lib.git')
 
-local nvim_pluing_dir_start = os.getenv("HOME") .. "/.config/nvim/pack/bundle/start"
+local nvim_dir = os.getenv("HOME") .. "/.config/nvim"
+local nvim_pluing_dir_start = nvim_dir .. "/pack/bundle/start"
+
+configz.link(nvim_dir .. "/snippets", {
+  source = os.getenv("PWD") .. "/site-modules/core/files/vim/snippets/snippets"
+})
 
 git.repo({
   src = "https://github.com/sbdchd/neoformat",
