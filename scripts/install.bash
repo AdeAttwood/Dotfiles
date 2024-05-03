ded2d4e66bafa275d30769e20955f2fe8af441da
Ade Attwood <code@adeattwood.co.uk>
2024-05-03 07:12:19 +0100

    fix(core): issues with the install script
    For the zypper install script, there was a spelling mistake in
    `libopenssl-devel`, this is required for compiling configz.
    
    On WSL git is not installed by default, this now installs it so we can
    download the stuff.
    
    In the conditions to see if packages are installed, there are no quotes
    around the commands. This is then giving false positives and not
    installing configz or the rust toolchain. This has now been sorted so
    its all good.

--- scripts/install.bash
+++ scripts/install.bash
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ -x $(command -v zypper) ]; then
+if [ -x "$(command -v zypper)" ]; then
   sudo zypper install --type pattern devel_basis
-  sudo zypper install libopenssl-deve
+  sudo zypper install libopenssl-devel git
 else
   echo "ERROR: Package manager not found. Could not install"
 fi
 
-if [ ! -x $(command -v cargo) ]; then
+if [ ! -x "$(command -v cargo)" ]; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
   source "$HOME/.cargo/env"
 fi
 
-if [ ! -x $(command -v configz) ]; then
+if [ ! -x "$(command -v configz)" ]; then
   cargo install --git https://github.com/AdeAttwood/Configz
 fi
 
